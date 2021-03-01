c73dc25fc2d37cfd926ee642a9f5df42b3822d8d
schnee25 <schnee.2114@gmail.com>
2021-03-02 04:35:31 +0900

    for文の練習

---
+++ for.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ループ(for)
+*/
+
+int main(void)
+{
+    int m;
+    for (m = 0; m < 10; m++)
+    {
+        printf("m: %d\n", m);
+    }
+
+    /*
+     int m = 0;
+    while (m < 10)
+    {
+        printf("m: %d\n", m);
+        m++;
+    }
+    */
+}
