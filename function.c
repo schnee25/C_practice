0348015c49c8a9dd27cac95d8bccd683ae1cfebc
schnee25 <schnee.2114@gmail.com>
2021-03-11 18:11:44 +0900

    float型の説明を追記

--- function.c
+++ function.c
@@ -21,7 +21,16 @@ float getMax(float a, float b)
 int main(void)
 {
     float result;
-    getMax(2.3, 5.2);
+    result = getMax(2.3, 5.2);
     printf("%f\n", result);
     return 0;
 }
+
+/*float型は4バイト＝32ビットのサイズ
+符号部 1bit
+指数部 8bit
+仮数部 23bit
+有効桁数は二進法で24、10進法では約7桁*/
+
+/*ある数を、A×BC という形で表現したとき、
+A を仮数部、B を基数、C を指数部と言う*/
